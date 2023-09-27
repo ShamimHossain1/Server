89462e2686dacf27b29907818b185b2813f24cd2
ShamimHossain1 <beastb697@gmail.com>
2023-09-27 21:17:41 +0600

    'post'

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+
+const users =[
+    {id:1, name:'shamim', email:'shamim@gmailcom'},
+    {id:2, name:'sauda', email:'sauda@gmailcom'},
+]
+
+
 app.get('/', (req, res) => {
     res.send('users management server is running')
+})
+
+app.get('/users', (req, res)=>{
+    res.send(users);
+
+})
+
+app.post('/users', (req, res)=>{
+    console.log(req.body)
+    const newUser = req.body;
+    newUser.id= users.length + 1;
+    users.push(newUser)
+    res.send(newUser);
 })
  
 app.listen(port, () => {
