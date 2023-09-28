5012fa87f756e5d0a6b2e2722a04720973a92408
ShamimHossain1 <beastb697@gmail.com>
2023-09-28 21:16:44 +0600

    3

--- index.js
+++ index.js
@@ -7,29 +7,13 @@ app.use(cors());
 app.use(express.json());
 
 
-const users =[
-    {id:1, name:'shamim', email:'shamim@gmailcom'},
-    {id:2, name:'sauda', email:'sauda@gmailcom'},
-]
+
 
 
 app.get('/', (req, res) => {
     res.send('users management server is running')
 })
 
-app.get('/users', (req, res)=>{
-    res.send(users);
-
-})
-
-app.post('/users', (req, res)=>{
-    console.log(req.body)
-    const newUser = req.body;
-    newUser.id= users.length + 1;
-    users.push(newUser)
-    res.send(newUser);
-})
- 
 app.listen(port, () => {
     console.log(`server is running on PORT:${port}`)
 })
